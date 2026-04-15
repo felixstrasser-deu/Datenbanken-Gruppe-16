5450f61389e0ec1444a053ec1c030a6c30b901b5
Mhdhbwbe <hamm.magdalena-24@stud.dhbw-ravensburg.de>
2026-04-15 09:34:46 +0200

    Startseite

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Verwaltung von Radrennen</title>
+</head>
+<body>
+
+    <h1>Verwaltung von Radrennen</h1>
+
+    <p>Das ist ein Beispieltext für deine HTML-Seite.</p>
+
+    <h2>Über uns</h2>
+    <p>Wir sind eine Gruppe und arbeiten gemeinsam an einem Projekt.</p>
+
+    <h3>Unsere Ziele</h3>
+    <ul>
+        <li>Programmieren lernen</li>
+        <li>Mit GitHub arbeiten</li>
+        <li>Ein cooles Projekt erstellen</li>
+    </ul>
+
+    <p>Mehr Infos findest du bald hier!</p>
+
+</body>
+</html>
