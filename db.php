779aaadffa1c4cf0ba2afaf5b53e13bcb72d694d
Mhdhbwbe <hamm.magdalena-24@stud.dhbw-ravensburg.de>
2026-04-15 08:59:13 +0200

    DB

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+$host = "localhost";
+$user = "gruppe16";
+$password = "Bq+7wpK9;J$?";
+$db = "gruppe16";
+
+$connection = mysqli_connect($host, $user, $password);
+mysqli_select_db($connection, $db);
+
+?>
