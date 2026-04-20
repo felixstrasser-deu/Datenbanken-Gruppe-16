8f3c835f21b0a25bc6b65b2dbe78e9079dfa0208
felixstrasser-deu <felix.strasser@mail.de>
2026-04-20 11:46:43 +0200

    Login-Box

--- login.php
+++ login.php
@@ -1,1 +1,21 @@
-hshshs
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <style>
+        body{
+            front-family: Arial;
+            background-colour: #f4f6f8;
+
+        }
+
+    login.box{
+        background: white;
+            padding: 30px;
+            border-radius: 10px;
+            width: 300px;
+            box-shadow: 0 5px 15px rgba(0,0,0,0.1);
+    }
