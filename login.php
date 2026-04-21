ec0665fd1aacbdcabb443d24b63d6158fffe8624
felixstrasser-deu <felix.strasser@mail.de>
2026-04-21 09:01:24 +0200

    Weiterleitung registrierung

--- login.php
+++ login.php
@@ -11,13 +11,15 @@
 
 <form>
     <label>Teamchef:</label><br>
-    <input type="text"><br><br>
+    <input type="text"placeholder = "Loginname"><br><br>
 
     <label>Passwort:</label><br>
     <input type="password"placeholder = "Passwort"><br><br>
     <div style="display: flex; gap: 10px;">
-     <button type="submit">Login</button>
+ <form action="registrieren.php">
      <button type="submit">Registrieren</button>
+</form>
+     <button type="submit">Login</button>
 </div>
 </form>
 </body>
