658bfc30e79bcb8e9108858cb3ccb0003b3f3f1d
Mhdhbwbe <hamm.magdalena-24@stud.dhbw-ravensburg.de>
2026-04-21 17:39:50 +0200

    Trainings anlegen

--- training.php
+++ training.php
@@ -9,22 +9,34 @@
 <body>
     <h2>Training</h2>
 
-<label for="ziel">Trainingsziel:</label>
-<select id="ziel" name="ziel" required>
-    <option value="ziel1">Ausdauer</option>
-    <option value="ziel2">Sprintkraft</option>
-    <option value="ziel3">Steigungen</option>
-</select>
+<?php
+include 'db.php';
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $datum = $_POST['datum'];
+    $kilometer = $_POST['km'];
+    $trainingsziel = $_POST['ziel'];
+    $mitarbeiter = $_POST['fahrer'];
+    $team = 1; // Einfach hartcodiert für Anfänger
+
+    $sql = "INSERT INTO Training (Datum, Kilometer, Trainingsziel, Team, Mitarbeiter) VALUES ('$datum', $kilometer, '$trainingsziel', $team, '$mitarbeiter')";
+    mysqli_query($connection, $sql);
+    echo "<p>Training gespeichert!</p>";
+}
+?>
 
 <form method="post" action="training.php">
-    <label> Fahrer:</label>
-    <select name="fahrer" required>
+    <label for="fahrer">Fahrer:</label>
+    <select name="fahrer" id="fahrer" required>
         <option value="fahrer1">Fahrer 1</option>
         <option value="fahrer2">Fahrer 2</option>
         <option value="fahrer3">Fahrer 3</option>
+    </select>
+    
+    <br>
+
     <label for="datum">Datum:</label>
-    <input type="date" name="datum" required>
+    <input type="date" name="datum" id="datum" required>
     
     <br>
 
@@ -33,8 +45,10 @@
     
     <br>
 
-    <label for="kilometer">Kilometer:</label>
+    <label for="km">Kilometer:</label>
     <input type="number" id="km" name="km" required>
+
+    <br>
 
     <label for="ziel">Trainingsziel:</label>
     <select id="ziel" name="ziel" required>
@@ -42,6 +56,8 @@
         <option value="ziel2">Sprintkraft</option>
         <option value="ziel3">Steigungen</option>
     </select>
+
+    <br>
 
     <button type="submit">Training erfassen</button>
 </form>
